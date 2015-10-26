aac8618db581cfc7a730a7daf84565b4b7861bea
Manu Lohiya <manulohiya@gmail.com>
2015-10-25 18:22:07 -0700

    wip

--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'ngRoute'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  	.when('/', {
+  		templateUrl: 'templates/home.html'
+  	})
+  
+}])
+
+;
